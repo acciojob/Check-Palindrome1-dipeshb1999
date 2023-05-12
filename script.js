8d0ab4cd2631acb91717820d175ad7769b0c6a7b
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-12 15:38:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
-function palindrome(str){
+// function palindrome(str){
 
+// }
+// module.exports = palindrome
+function isPalindrome(s) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanString = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+  // Compare characters from both ends
+  for (let i = 0, j = cleanString.length - 1; i < j; i++, j--) {
+    if (cleanString[i] !== cleanString[j]) {
+      return false;
+    }
+  }
+
+  return true;
 }
-module.exports = palindrome
+
